3073334fc45b57ab356182dbd6de56a1d6e21d81
John <johnsnyder7@gmail.com>
2015-12-04 21:13:47 -0500

    moog stylez

--- app/assets/javascripts/components/organ.js.jsx
+++ app/assets/javascripts/components/organ.js.jsx
@@ -7,37 +7,39 @@ getInitialState: function () {
     octave = this.state.octave;
     return (
       <div className="organ">
+        <div className="moog" style={{backgroundImage: 'url(' + window.subPar.images.moog + ')'}}>
+          <ul className="keys group">
+            <div className="allkeys group">
+            {Object.keys(TONES).map(function (key) {
+              if (key.slice(1) === "sharp" || key.slice(2) === "sharp") {
+
+                return <li className="key sharp" id={key}>
+                        <Key gain={0.1} realNoteName={key} noteName={ TONES[key] * 6 } octave={octave} />
+                        <Key gain={0.1} realNoteName={key} noteName={ TONES[key] * 5 } octave={octave} />
+                        <Key gain={0.1} realNoteName={key} noteName={ TONES[key] * 4 + 3 } octave={octave} />
+                        <Key gain={0.2} realNoteName={key} noteName={ TONES[key] * 2 } octave={octave} />
+                        <Key gain={0.1} realNoteName={key} noteName={ TONES[key] * 2 + 2} octave={octave} />
+                        <Key gain={0.3} show={true} realNoteName={key} noteName={TONES[key]} octave={octave} />
+                      </li>;
+
+              } else {
+
+                return <li className="key" id={key}>
+                        <Key gain={0.1} realNoteName={key} noteName={ TONES[key] * 6 } octave={octave} />
+                        <Key gain={0.1} realNoteName={key} noteName={ TONES[key] * 5 } octave={octave} />
+                        <Key gain={0.1} realNoteName={key} noteName={ TONES[key] * 4 + 3 } octave={octave} />
+                        <Key gain={0.2} realNoteName={key} noteName={ TONES[key] * 2 } octave={octave} />
+                        <Key gain={0.1} realNoteName={key} noteName={ TONES[key] * 2 + 2} octave={octave} />
+                        <Key gain={0.3} show={true} realNoteName={key} noteName={TONES[key]} octave={octave} />
+                      </li>;
+
+                  }
+            })}
+              </div>
+            </ul>
+          </div>
+
         <Recorder />
-        <ul className="keys group">
-          <div className="allkeys group">
-          {Object.keys(TONES).map(function (key) {
-            if (key.slice(1) === "sharp" || key.slice(2) === "sharp") {
-
-              return <li className="key sharp" id={key}>
-                      <Key gain={0.1} realNoteName={key} noteName={ TONES[key] * 6 } octave={octave} />
-                      <Key gain={0.1} realNoteName={key} noteName={ TONES[key] * 5 } octave={octave} />
-                      <Key gain={0.1} realNoteName={key} noteName={ TONES[key] * 4 + 3 } octave={octave} />
-                      <Key gain={0.2} realNoteName={key} noteName={ TONES[key] * 2 } octave={octave} />
-                      <Key gain={0.1} realNoteName={key} noteName={ TONES[key] * 2 + 2} octave={octave} />
-                      <Key gain={0.3} show={true} realNoteName={key} noteName={TONES[key]} octave={octave} />
-                    </li>;
-
-            } else {
-
-              return <li className="key" id={key}>
-                      <Key gain={0.1} realNoteName={key} noteName={ TONES[key] * 6 } octave={octave} />
-                      <Key gain={0.1} realNoteName={key} noteName={ TONES[key] * 5 } octave={octave} />
-                      <Key gain={0.1} realNoteName={key} noteName={ TONES[key] * 4 + 3 } octave={octave} />
-                      <Key gain={0.2} realNoteName={key} noteName={ TONES[key] * 2 } octave={octave} />
-                      <Key gain={0.1} realNoteName={key} noteName={ TONES[key] * 2 + 2} octave={octave} />
-                      <Key gain={0.3} show={true} realNoteName={key} noteName={TONES[key]} octave={octave} />
-                    </li>;
-
-                }
-          })}
-            </div>
-          </ul>
-
           <Jukebox />
 
         </div>
